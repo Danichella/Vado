be46ed6411a64b96e57dc4bb87b72df732feea73
Danichella <daniladun07@gmail.com>
2024-05-03 17:52:59 +0300

    Added device with google oauth

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[google_oauth2]
+  
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.find_by(email: data['email'])
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(
+            email: data['email'],
+            password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
+end
